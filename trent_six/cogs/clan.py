a3a4b9ae9dc2dfd409866ca671ae65d47aa08512
Mike Hennessy <henworth@henabytes.com>
2019-05-10 23:20:59 -0400

    Ensure gamertags with spaces can be approved

--- trent_six/cogs/clan.py
+++ trent_six/cogs/clan.py
@@ -116,55 +116,58 @@ class ClanCog(commands.Cog, name='Clan'):
     @is_registered()
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
-    async def approve(self, ctx, gamertag):
-        async with ctx.typing():
-            member_db = await self.bot.database.get_member_by_discord_id(ctx.author.id)
-            clan_db = await self.bot.database.get_clan_by_guild(ctx.guild.id)
+    async def approve(self, ctx, *args):
+        await ctx.trigger_typing()
+        gamertag = ' '.join(args)
 
-            try:
-                player = await self.bot.destiny.api.search_destiny_player(
-                    destiny_constants.PLATFORM_XBOX, gamertag
-                )
-            except pydest.PydestException:
-                await ctx.send(f"Invalid gamertag {gamertag}")
-                return
+        member_db = await self.bot.database.get_member_by_discord_id(ctx.author.id)
+        clan_db = await self.bot.database.get_clan_by_guild(ctx.guild.id)
 
-            membership_id = None
-            for membership in player['Response']:
-                if membership['membershipType'] == destiny_constants.PLATFORM_XBOX and membership['displayName'] == gamertag:
-                    membership_id = membership['membershipId']
-                    break
+        try:
+            player = await self.bot.destiny.api.search_destiny_player(
+                destiny_constants.PLATFORM_XBOX, gamertag
+            )
+        except pydest.PydestException:
+            await ctx.send(f"Invalid gamertag {gamertag}")
+            return
 
-            if not membership_id:
-                await ctx.send(f"Could not find Destiny player for gamertag {gamertag}")
-                return
+        membership_id = None
+        for membership in player['Response']:
+            if membership['membershipType'] == destiny_constants.PLATFORM_XBOX and membership['displayName'] == gamertag:
+                membership_id = membership['membershipId']
+                break
 
-            try:
-                res = await self.bot.destiny.api.group_approve_pending_member(
-                    group_id=clan_db.clan_id,
-                    membership_type=destiny_constants.PLATFORM_XBOX,
-                    membership_id=membership_id,
-                    message=f"Welcome to {clan_db.name}!",
-                    access_token=member_db.bungie_access_token
-                )
-            except pydest.PydestTokenException:
-                tokens = await self.bot.destiny.api.refresh_oauth_token(
-                    member_db.bungie_refresh_token
-                )
-                res = await self.bot.destiny.api.group_approve_pending_member(
-                    group_id=clan_db.clan_id,
-                    membership_type=destiny_constants.PLATFORM_XBOX,
-                    membership_id=membership_id,
-                    message=f"Welcome to {clan_db.name}!",
-                    access_token=tokens['access_token']
-                )
-                member_db.bungie_access_token = tokens['access_token']
-                member_db.bungie_refresh_token = tokens['refresh_token']
-                await self.bot.database.update(member_db)
+        if not membership_id:
+            await ctx.send(f"Could not find Destiny player for gamertag {gamertag}")
+            return
 
-            # if res['ErrorStatus'] == 'ClanTargetDisallowsInvites':
-            #     message = f"User **{gamertag}** has disabled clan invites"
-            # else:
+        try:
+            res = await self.bot.destiny.api.group_approve_pending_member(
+                group_id=clan_db.clan_id,
+                membership_type=destiny_constants.PLATFORM_XBOX,
+                membership_id=membership_id,
+                message=f"Welcome to {clan_db.name}!",
+                access_token=member_db.bungie_access_token
+            )
+        except pydest.PydestTokenException:
+            tokens = await self.bot.destiny.api.refresh_oauth_token(
+                member_db.bungie_refresh_token
+            )
+            res = await self.bot.destiny.api.group_approve_pending_member(
+                group_id=clan_db.clan_id,
+                membership_type=destiny_constants.PLATFORM_XBOX,
+                membership_id=membership_id,
+                message=f"Welcome to {clan_db.name}!",
+                access_token=tokens['access_token']
+            )
+            member_db.bungie_access_token = tokens['access_token']
+            member_db.bungie_refresh_token = tokens['refresh_token']
+            await self.bot.database.update(member_db)
+
+        if res['ErrorStatus'] != 'Success':
+            message = f"Could not approve **{gamertag}**"
+            logging.error(f"Could not approve \"{gamertag}\": {res}")
+        else:
             message = f"Approved **{gamertag}** as a member of clan **{clan_db.name}**"
 
         await ctx.send(message)
