e70faa76f0a3ac068252efdd1b74290c8ec4216c
Mike Hennessy <henworth@henabytes.com>
2020-01-10 10:26:12 -0500

    Fix sherpa discord role assignment
    Previously the search was not limited by discord guild, so if a sherpa
    was a member of two or more guilds their sherpa status would be
    set by the first guild scanned. This resulted in inconsistent behavior
    in the scan/tag process.

--- seraphsix/tasks/discord.py
+++ seraphsix/tasks/discord.py
@@ -1,7 +1,8 @@
 import discord
 import logging
 
-from seraphsix.database import ClanMember, Guild, Member, Role
+from peewee import DoesNotExist
+from seraphsix.database import Clan, ClanMember, Guild, Member, Role
 
 logging.getLogger(__name__)
 
@@ -22,7 +23,9 @@ async def store_sherpas(bot, guild):
     sherpas_discord = await find_sherpas(bot, guild)
     sherpas_discord_ids = [sherpa.id for sherpa in sherpas_discord]
 
-    query = Member.select(Member.discord_id).join(ClanMember).where(ClanMember.is_sherpa)
+    query = Member.select(Member.discord_id).join(ClanMember).join(Clan).join(Guild).where(
+        (ClanMember.is_sherpa) & (Guild.id == guild.id)
+    )
     sherpas_db = await bot.database.execute(query)
     sherpas_db_ids = [sherpa_db.discord_id for sherpa_db in sherpas_db]
 
@@ -32,13 +35,16 @@ async def store_sherpas(bot, guild):
     sherpas_added = list(discord_set - db_set)
     sherpas_removed = list(db_set - discord_set)
 
+    base_member_query = ClanMember.select(ClanMember.id).join(Member)
     if sherpas_added:
-        members = ClanMember.select(ClanMember.id).join(Member).where(Member.discord_id << sherpas_added)
+        logging.info(f"Sherpas added for {guild.id}: {sherpas_added}")
+        members = base_member_query.where(Member.discord_id << sherpas_added)
         query = ClanMember.update(is_sherpa=True).from_(members).where(ClanMember.id << members)
         await bot.database.execute(query)
 
     if sherpas_removed:
-        members = ClanMember.select(ClanMember.id).join(Member).where(Member.discord_id << sherpas_removed)
+        logging.info(f"Sherpas removed for {guild.id}: {sherpas_removed}")
+        members = base_member_query.where(Member.discord_id << sherpas_removed)
         query = ClanMember.update(is_sherpa=False).from_(members).where(ClanMember.id << members)
         await bot.database.execute(query)
 
@@ -52,9 +58,14 @@ async def update_sherpa(bot, before, after):
     roles_db = await bot.database.execute(roles_query)
     role_db_ids = set([role.role_id for role in roles_db])
 
-    member_query = ClanMember.select(ClanMember.id).join(Member).where(Member.discord_id == after.id)
+    try:
+        member_query = ClanMember.select(ClanMember).join(Member).where(Member.discord_id == after.id)
+        member_db = await bot.database.get(member_query)
+    except DoesNotExist:
+        return
+
     if role_ids.intersection(role_db_ids):
-        query = ClanMember.update(is_sherpa=True).from_(member_query)
+        member_db.is_sherpa = True
     else:
-        query = ClanMember.update(is_sherpa=False).from_(member_query)
-    await bot.database.execute(query)
+        member_db.is_sherpa = False
+    await bot.database.update(member_db)
