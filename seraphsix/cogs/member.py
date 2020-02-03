df3dee6a703732126268cccd6bb1e0c3ad1fe44a
Mike Hennessy <henworth@henabytes.com>
2020-02-03 16:54:12 -0500

    Remove whitespace

--- seraphsix/cogs/member.py
+++ seraphsix/cogs/member.py
@@ -297,7 +297,7 @@ Example: ?member sherpatime
                 try:
                     member_discord = await commands.MemberConverter().convert(ctx, member_name)
                     member_db = await self.bot.database.get_member_by_discord_id(member_discord.id)
-                except (BadArgument, DoesNotExist):                
+                except (BadArgument, DoesNotExist):
                     return await manager.send_and_clean(f"Invalid member name `{member_name}`")
                 else:
                     member_name = member_discord.display_name
